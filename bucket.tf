db1bd5929b60d887a4597f663c5266869fe9d81e
YoussefMobarak1702 <ym17021995@gmail.com>
2025-01-25 23:57:08 +0200

    update

---
+++ bucket.tf
@@ -0,0 +1,8 @@
+resource "google_storage_bucket" "tbuk" {
+  project = var.project
+  name = "${var.data-project}-tbuk"
+  force_destroy = false
+  uniform_bucket_level_access = true
+  location = var.region
+  labels = local.labels
+}
---
+++ variables.tf
@@ -0,0 +1,23 @@
+locals {
+    labels = {
+        "data-project" = var.data-project
+    }
+}
+
+variable "project" {
+    type= string
+    description = "ID Google project"
+    default = "cohesive-mender-448920-c6"
+}
+
+variable "region" {
+    type= string
+    description = "Region Google project"
+    default = "us-east1"
+}
+
+variable  "data-project" {
+    type = string
+    description = "Name of data pipeline project to use as resource prefix"
+    default = "test"
+}
